0dec9cdae49fc42de3e56f0fdb8b9af18625ca54
Mike Su <kyatapi@gmail.com>
2017-06-07 07:13:46 +0800

    Add main logic and dependency class "point"

--- forth_point.cpp
+++ forth_point.cpp
@@ -1,14 +1,41 @@
+#include <vector>
+#include <iostream>
+#include <iomanip>
 
-#ifndef ONLINE_JUDGE
-#include "test_cases.h"
-extern void run_test(int argc, char **argv);
-#else
-inline void run_test(int argc, char **argv) {}
-#endif // !ONLINE_JUDGE
+namespace forth_point {
+	class point {
+	public:
+		point() : x(0), y(0) {}
+		point(double _x, double _y) : x(_x), y(_y) {}
+		point(const point& rhs) : x(rhs.x), y(rhs.y) {}
+		~point() {}
+
+		point& operator=(const point& rhs) { x = rhs.x; y = rhs.y; return *this; }
+		const double getx() const { return x; }
+		const double gety() const { return y; }
+	private:
+		double x;
+		double y;
+	};
+}
+
+using namespace std;
+
+vector<forth_point::point> read_parallelogram(std::istream& is) {
+	vector<forth_point::point> parallelogram;
+	return parallelogram;
+}
+
+forth_point::point calculate_forth_point(vector<forth_point::point>& points) {
+	return forth_point::point();
+}
 
 int main(int argc, char **argv) {
-	run_test(argc, argv);
-
+	while (!cin.eof()) {
+		vector<forth_point::point> parallelogram = read_parallelogram(cin);
+		forth_point::point forth = calculate_forth_point(parallelogram);
+		cout << fixed << setprecision(3) << forth.getx() << " " << fixed << setprecision(3) << forth.gety() << endl;
+	}
 
 	return 0;
 }
